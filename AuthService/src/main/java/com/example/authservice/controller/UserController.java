77e46bca50776bdadfb549952b8ccca860488461
LAPTOP-6D3NOR16\Developer <isohyeon0816@gmail.com>
2023-02-01 21:31:10 +0900

    feat: Auth 서비스 통신 확인

--- AuthService/src/main/java/com/example/authservice/controller/UserController.java
+++ AuthService/src/main/java/com/example/authservice/controller/UserController.java
@@ -3,11 +3,10 @@ package com.example.authservice.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
 public class UserController {
 
-    @PostMapping("/signin")
-    public String authenticate(@RequestHeader(value = "token") String token) {
-        return "token is " + token;
+    @GetMapping("/login")
+    public String authenticate() {
+        return "hello";
     }
 }
---
+++ AuthService/src/main/java/com/example/authservice/domain/entity/User.java
@@ -0,0 +1,5 @@
+package com.example.authservice.domain.entity;
+
+public class User {
+
+}
